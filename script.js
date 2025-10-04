2949dc334513e2e601a3c8930cc6dcfae79135f5
Mohamed Adel <124206812+m-adel-tec@users.noreply.github.com>
2025-10-05 00:32:09 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+function openProject(id){
+  if(id === 'uc') window.open('https://m-adel-tec.github.io/m-adel','_blank');
+  if(id === 'sr') alert('Project demo available on request.');
+}
